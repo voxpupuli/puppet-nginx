1a3d758d4bee2386bbbe1e08c38d2ada3de8bf09
Jeff McCune <jeff@puppetlabs.com>
2011-06-15 09:59:32 -0700

    Update init.pp test with relationships
    This shows the module user how relationships can be easily added
    to the main nginx class without peeking inside the guts of the module.

--- tests/init.pp
+++ tests/init.pp
@@ -1,2 +1,9 @@
-include nginx
-
+# The notify before should always come BEFORE all resources
+# managed by the nginx class
+# and the notify last should always come AFTER all resources
+# managed by the nginx class.
+node default {
+  notify { 'before': }
+  -> class { 'nginx': }
+  -> notify { 'last': }
+}
