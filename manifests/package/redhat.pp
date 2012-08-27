49cc046ac81a4dba86f342105b32884e3e0615d4
James Fryman <james@frymanet.com>
2012-08-27 09:26:48 -0500

    addding yumrepo for RHEL systems to ensure latest from NGINX

--- manifests/package/redhat.pp
+++ manifests/package/redhat.pp
@@ -15,7 +15,16 @@
 # This class file is not called directly
 class nginx::package::redhat {
   $redhat_packages = ['nginx', 'GeoIP', 'gd', 'libXpm', 'libxslt']
+
+  yumrepo { "nginx-release":
+    baseurl  => 'http://nginx.org/packages/OS/OSRELEASE/$basearch/'
+    descr    => 'nginx repo',
+    enabled  => '1',
+    gpgcheck => '0',
+  }
+
   package { $redhat_packages:
-    ensure => present,
+    ensure  => present,
+    require => Yumrepo['nginx-release'],
   }
 }
