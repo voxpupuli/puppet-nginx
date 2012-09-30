98fc7923c45f0b7d599f76dc0f523188f921f3e8
Guzmán Brasó <guzman@guruhub.com.uy>
2012-09-30 05:30:04 -0300

    Added purge support in confd.
    Added confd_purge option to tell it to purge files non managed by pupet in conf.d, default is false.

--- manifests/init.pp
+++ manifests/init.pp
@@ -32,6 +32,7 @@ class nginx (
 	$worker_processes	= $nginx::params::nx_worker_processes,
 	$worker_connections	= $nginx::params::nx_worker_connections,
 	$proxy_set_header	= $nginx::params::nx_proxy_set_header
+        $confd_purge            = $nginx::params::nx_confd_purge,
 ) inherits nginx::params {
 
   include stdlib
@@ -44,6 +45,7 @@ class nginx (
     worker_processes 	=> $worker_processes,
     worker_connections 	=> $worker_connections,
     proxy_set_header 	=> $proxy_set_header,
+    confd_purge         => $confd_purge,
     require 		=> Class['nginx::package'],
     notify  		=> Class['nginx::service'],
   }
