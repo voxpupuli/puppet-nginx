91165742b60fbd8fa8fc291c8b87207c89da3249
James Fryman <james@frymanet.com>
2012-10-04 15:17:31 -0500

    fixing dangling commas at the end of class parameters

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,7 +17,7 @@ class nginx::config(
   $worker_processes    = $nginx::params::nx_worker_processes,
   $worker_connections  = $nginx::params::nx_worker_connections,
   $proxy_set_header    = $nginx::params::nx_proxy_set_header,
-  $confd_purge         = $nginx::params::nx_confd_purge,
+  $confd_purge         = $nginx::params::nx_confd_purge
 ) inherits nginx::params {
   File {
     owner => 'root',
--- manifests/service.pp
+++ manifests/service.pp
@@ -15,7 +15,7 @@
 # This class file is not called directly
 class nginx::service(
   $configtest_enable   = $nginx::params::nx_configtest_enable,
-  $service_restart     = $nginx::params::nx_service_restart,
+  $service_restart     = $nginx::params::nx_service_restart
 ) {
   exec { 'rebuild-nginx-vhosts':
     command     => "/bin/cat ${nginx::params::nx_temp_dir}/nginx.d/* > ${nginx::params::nx_conf_dir}/conf.d/vhost_autogen.conf",
