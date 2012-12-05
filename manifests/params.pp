5947606db5850767c8210c5740cd8721dabb58b2
Drew Blessing <Drew.Blessing@Buckle.com>
2012-12-05 09:39:41 -0600

    Change params

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,11 +18,14 @@ class nginx::params {
   $nx_run_dir  = '/var/nginx'
 
   $nx_conf_dir           = '/etc/nginx'
-  $nx_worker_processes   = 1
+#  $nx_worker_processes   = 1
+#  $nx_worker_connections = 1024
+  $nx_worker_processes   = 2
   $nx_worker_connections = 1024
   $nx_multi_accept       = 'off'
   $nx_sendfile           = 'on'
-  $nx_keepalive_timeout  = 65
+  #$nx_keepalive_timeout  = 65
+  $nx_keepalive_timeout  = 90
   $nx_tcp_nodelay        = 'on'
   $nx_gzip               = 'on'
 
