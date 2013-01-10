fa3d75cf274d27d93dfd806d56bff3e543b0400e
Drew Blessing <Drew.Blessing@Buckle.com>
2013-01-09 19:25:45 -0600

    Add log format option to vhost

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,7 +17,9 @@ class nginx::config(
   $worker_processes    = $nginx::params::nx_worker_processes,
   $worker_connections  = $nginx::params::nx_worker_connections,
   $proxy_set_header    = $nginx::params::nx_proxy_set_header,
-  $confd_purge         = $nginx::params::nx_confd_purge
+  $confd_purge         = $nginx::params::nx_confd_purge,
+  $http_cfg_prepend    = undef,
+  $http_cfg_append     = undef
 ) inherits nginx::params {
   File {
     owner => 'root',
