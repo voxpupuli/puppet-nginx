4bcb0993c180b94c7d9a3f122b14068a2420f31b
Sander Hoentjen <sander@hoentjen.eu>
2013-05-08 14:15:42 +0200

    linting

--- tests/vhost.pp
+++ tests/vhost.pp
@@ -2,15 +2,15 @@ include nginx
 
 nginx::resource::vhost { 'test.local':
   ensure       => present,
-  ipv6_enable  => 'true',
+  ipv6_enable  => true,
   proxy        => 'http://proxypass',
 }
 
 nginx::resource::vhost { 'test.local:8080':
+  ensure       => present,
   listen_port  => 8080,
   server_name  => 'test.local',
-  ensure       => present,
-  ipv6_enable  => 'true',
+  ipv6_enable  => true,
   proxy        => 'http://proxypass',
 }
 
