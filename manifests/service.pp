3231b44cc5966aa514fb91b7a3a36f0338c13df2
Thomas Thompson <tthompson@nexon.net>
2013-07-15 10:45:38 -0700

    allow all params to be overridden

--- manifests/service.pp
+++ manifests/service.pp
@@ -18,16 +18,16 @@ class nginx::service(
   $service_restart   = $nginx::params::nx_service_restart
 ) {
   exec { 'rebuild-nginx-vhosts':
-    command     => "/bin/cat ${nginx::params::nx_temp_dir}/nginx.d/* > ${nginx::params::nx_conf_dir}/conf.d/vhost_autogen.conf",
+    command     => "/bin/cat ${nginx::temp_dir}/nginx.d/* > ${nginx::conf_dir}/conf.d/vhost_autogen.conf",
     refreshonly => true,
-    unless      => "/usr/bin/test ! -f ${nginx::params::nx_temp_dir}/nginx.d/*",
-    subscribe   => File["${nginx::params::nx_temp_dir}/nginx.d"],
+    unless      => "/usr/bin/test ! -f ${nginx::temp_dir}/nginx.d/*",
+    subscribe   => File["${nginx::temp_dir}/nginx.d"],
   }
   exec { 'rebuild-nginx-mailhosts':
-    command     => "/bin/cat ${nginx::params::nx_temp_dir}/nginx.mail.d/* > ${nginx::params::nx_conf_dir}/conf.mail.d/vhost_autogen.conf",
+    command     => "/bin/cat ${nginx::temp_dir}/nginx.mail.d/* > ${nginx::conf_dir}/conf.mail.d/vhost_autogen.conf",
     refreshonly => true,
-    unless      => "/usr/bin/test ! -f ${nginx::params::nx_temp_dir}/nginx.mail.d/*",
-    subscribe   => File["${nginx::params::nx_temp_dir}/nginx.mail.d"],
+    unless      => "/usr/bin/test ! -f ${nginx::temp_dir}/nginx.mail.d/*",
+    subscribe   => File["${nginx::temp_dir}/nginx.mail.d"],
   }
   service { 'nginx':
     ensure     => running,
