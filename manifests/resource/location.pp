06f50454cbb4c3c9658369d7f460aad8ec4bfae3
Justice London <jlondon@syrussystems.com>
2013-07-19 12:07:41 -0400

    Small doc fix

--- manifests/resource/location.pp
+++ manifests/resource/location.pp
@@ -14,6 +14,7 @@
 #   [*fastcgi*]              - location of fastcgi (host:port)
 #   [*fastcgi_params*]       - optional alternative fastcgi_params file to use
 #   [*fastcgi_script*]       - optional SCRIPT_FILE parameter
+#   [*fastcgi_split_path*]   - Allows settings of fastcgi_split_path_info so that you can split the script_name and path_info via regex
 #   [*ssl*]                  - Indicates whether to setup SSL bindings for this location.
 #   [*ssl_only*]             - Required if the SSL and normal vHost have the same port.
 #   [*location_alias*]       - Path to be used as basis for serving requests for this location
