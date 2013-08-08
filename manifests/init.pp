70207c9a6563c87823fa93ce4d87942e3a609fb8
Todd Eddy <vr@vrillusions.com>
2013-08-07 21:27:13 -0400

    Add logging customization to the main logs as well.

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,6 +44,8 @@ class nginx (
   $mail                   = $nginx::params::nx_mail,
   $server_tokens          = $nginx::params::nx_server_tokens,
   $http_cfg_append        = $nginx::params::nx_http_cfg_append,
+  $nginx_error_log        = $nginx::params::nx_nginx_error_log,
+  $http_access_log        = $nginx::params::nx_http_access_log,
   $nginx_vhosts           = {},
   $nginx_upstreams        = {},
   $nginx_locations        = {},
@@ -68,6 +70,8 @@ class nginx (
     confd_purge           => $confd_purge,
     server_tokens         => $server_tokens,
     http_cfg_append       => $http_cfg_append,
+    nginx_error_log       => $nginx_error_log,
+    http_access_log       => $http_access_log,
     require               => Class['nginx::package'],
     notify                => Class['nginx::service'],
   }
