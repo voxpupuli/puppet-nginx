4da3e462076dff6fd151e37817090ca11031f324
Omar Z <omarzt@gmail.com>
2013-09-05 23:15:45 +0100

    added proxy_buffer_size with default value of 8k

--- manifests/params.pp
+++ manifests/params.pp
@@ -58,6 +58,7 @@ class nginx::params {
   $nx_proxy_read_timeout      = '90'
   $nx_proxy_buffers           = '32 4k'
   $nx_proxy_http_version      = '1.0'
+  $nx_proxy_buffer_size       = '8k'
 
   $nx_logdir = $::kernel ? {
     /(?i-mx:linux)/ => '/var/log/nginx',
