cb635174609264e14bde5d5887134dd50b65de3b
Udo Waechter <u.waechter@mysportgroup.de>
2013-09-05 11:29:21 +0200

    utf-8 probs

--- manifests/resource/config.pp
+++ manifests/resource/config.pp
@@ -1,8 +1,11 @@
-define nginx::resource::config ($ensure = 'present', $config) {
+define nginx::resource::config (
+  $ensure   = 'present',
+  $config,
+  $filetype = 'conf') {
   include nginx::params
   validate_hash($config)
 
-  file { "${nginx::params::nx_conf_dir}/conf.d/${name}.conf":
+  file { "${nginx::params::nx_conf_dir}/conf.d/${name}.${filetype}":
     content => template('nginx/conf.d/config.erb'),
     ensure  => $ensure,
     notify  => Service['nginx'],
