eb3d6e5bb854feed97ae2e613daeb4991c57ad7a
Jarl Stefansson <jarl@sdelements.com>
2013-11-01 13:52:15 -0400

    Default ssl protocols are now set by nginx version
    nginx did not support TLS1.1/1.2 prior to version
    1.0.12, default ssl protocols should reflect this.

--- manifests/resource/vhost.pp
+++ manifests/resource/vhost.pp
@@ -42,7 +42,7 @@
 #   [*ssl_port*]            - Default IP Port for NGINX to listen with this SSL
 #     vHost on. Defaults to TCP 443
 #   [*ssl_protocols*]       - SSL protocols enabled. Defaults to 'SSLv3 TLSv1
-#     TLSv1.1 TLSv1.2'.
+#     TLSv1.1 TLSv1.2'. if nginx version >= 1.0.12 else 'SSLv3 TLSv1'
 #   [*ssl_ciphers*]         - SSL ciphers enabled. Defaults to
 #     'HIGH:!aNULL:!MD5'.
 #   [*spdy*]                - Toggles SPDY protocol.
@@ -102,7 +102,7 @@ define nginx::resource::vhost (
   $ssl_cert               = undef,
   $ssl_key                = undef,
   $ssl_port               = '443',
-  $ssl_protocols          = 'SSLv3 TLSv1 TLSv1.1 TLSv1.2',
+  $ssl_protocols          = undef,
   $ssl_ciphers            = 'HIGH:!aNULL:!MD5',
   $spdy                   = $nginx::params::nx_spdy,
   $proxy                  = undef,
@@ -147,6 +147,17 @@ define nginx::resource::vhost (
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
+  }
+
+  # SSL protocols, TLS1.1/1.2 were only added in nginx
+  # version 1.0.12, defaults should be correctly set
+  # depending on the nginx version
+  if ($ssl_protocols == undef) {
+    if versioncmp($nginx_version,'1.0.11') > 0 {
+      $ssl_protocols = 'SSLv3 TLSv1 TLSv1.1 TLSv1.2',
+    } else {
+      $ssl_protocols = 'SSLv3 TLSv1',
+    }
   }
 
   # Add IPv6 Logic Check - Nginx service will not start if ipv6 is enabled
