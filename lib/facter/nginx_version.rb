e51af1dd2f4ac510063b6c4914b7133a8225ba55
Jarl Stefansson <jarl@sdelements.com>
2013-11-05 11:54:35 -0500

    Better handling of nginx version fact

--- lib/facter/nginx_version.rb
+++ lib/facter/nginx_version.rb
@@ -1,7 +1,18 @@
+# Make nginx version available as a fact
 require 'puppet'
 pkg = Puppet::Type.type(:package).new(:name => "nginx")
 Facter.add("nginx_version") do
+  has_weight 100
   setcode do
-    /^(\d+\.\d+\.\d+).*$/.match(pkg.retrieve[pkg.property(:ensure)])[1]
+    Facter::Util::Resolution.exec('nginx -v')
+  end
+end
+
+Facter.add("nginx_version") do
+  has_weight 50
+  setcode do
+    if pkg.retrieve[pkg.property(:ensure)] != 'purged'
+        /^(\d+\.\d+\.\d+).*$/.match(pkg.retrieve[pkg.property(:ensure)])[1]
+    end
   end
 end
