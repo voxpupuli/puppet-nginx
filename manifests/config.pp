268144e0713616fc7aca1c5a9c9101256252c6b8
Maksym Melnychok <maksym.melnychok@gmail.com>
2014-03-07 15:52:38 +0100

    configuration revamp
    - make many more things configurable
    - stop using ::params::* for things that are configurable
    - add worker_rlimit_nofile option
    - add tcp_nopush option

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,28 +14,45 @@
 #
 # This class file is not called directly
 class nginx::config(
-  $worker_processes       = $nginx::params::nx_worker_processes,
-  $worker_connections     = $nginx::params::nx_worker_connections,
+  $client_body_temp_path  = $nginx::params::nx_client_body_temp_path,
+  $client_max_body_size   = $nginx::params::nx_client_max_body_size,
+  $conf_dir               = $nginx::params::nx_conf_dir,
   $confd_purge            = $nginx::params::nx_confd_purge,
-  $server_tokens          = $nginx::params::nx_server_tokens,
-  $proxy_set_header       = $nginx::params::nx_proxy_set_header,
-  $proxy_cache_path       = $nginx::params::nx_proxy_cache_path,
-  $proxy_cache_levels     = $nginx::params::nx_proxy_cache_levels,
-  $proxy_cache_keys_zone  = $nginx::params::nx_proxy_cache_keys_zone,
-  $proxy_cache_max_size   = $nginx::params::nx_proxy_cache_max_size,
-  $proxy_cache_inactive   = $nginx::params::nx_proxy_cache_inactive,
-  $proxy_http_version     = $nginx::params::nx_proxy_http_version,
+  $daemon_user            = $nginx::params::nx_daemon_user,
+  $events_use             = $nginx::params::nx_events_use,
+  $gzip                   = $nginx::params::nx_gzip,
+  $http_access_log        = $nginx::params::nx_http_access_log,
+  $http_cfg_append        = $nginx::params::nx_http_cfg_append,
+  $http_tcp_nodelay       = $nginx::params::nx_http_tcp_nodelay,
+  $http_tcp_nopush        = $nginx::params::nx_http_tcp_nopush,
+  $keepalive_timeout      = $nginx::params::nx_keepalive_timeout,
+  $logdir                 = $nginx::params::nx_logdir,
+  $mail                   = $nginx::params::nx_mail,
+  $multi_accept           = $nginx::params::nx_multi_accept,
   $names_hash_bucket_size = $nginx::params::nx_names_hash_bucket_size,
   $names_hash_max_size    = $nginx::params::nx_names_hash_max_size,
-  $types_hash_max_size    = $nginx::params::nx_types_hash_max_size,
-  $types_hash_bucket_size = $nginx::params::nx_types_hash_bucket_size,
-  $client_max_body_size   = $nginx::params::nx_client_max_body_size,
-  $proxy_buffers          = $nginx::params::nx_proxy_buffers,
-  $http_cfg_append        = $nginx::params::nx_http_cfg_append,
   $nginx_error_log        = $nginx::params::nx_nginx_error_log,
-  $http_access_log        = $nginx::params::nx_http_access_log,
+  $pid                    = $nginx::params::nx_pid,
   $proxy_buffer_size      = $nginx::params::nx_proxy_buffer_size,
-  $gzip                   = $nginx::params::nx_gzip,
+  $proxy_buffers          = $nginx::params::nx_proxy_buffers,
+  $proxy_cache_inactive   = $nginx::params::nx_proxy_cache_inactive,
+  $proxy_cache_keys_zone  = $nginx::params::nx_proxy_cache_keys_zone,
+  $proxy_cache_levels     = $nginx::params::nx_proxy_cache_levels,
+  $proxy_cache_max_size   = $nginx::params::nx_proxy_cache_max_size,
+  $proxy_cache_path       = $nginx::params::nx_proxy_cache_path,
+  $proxy_http_version     = $nginx::params::nx_proxy_http_version,
+  $proxy_set_header       = $nginx::params::nx_proxy_set_header,
+  $proxy_temp_path        = $nginx::params::nx_proxy_temp_path,
+  $run_dir                = $nginx::params::nx_run_dir,
+  $sendfile               = $nginx::params::nx_sendfile,
+  $server_tokens          = $nginx::params::nx_server_tokens,
+  $spdy                   = $nginx::params::nx_spdy,
+  $temp_dir               = $nginx::params::nx_temp_dir,
+  $types_hash_bucket_size = $nginx::params::nx_types_hash_bucket_size,
+  $types_hash_max_size    = $nginx::params::nx_types_hash_max_size,
+  $worker_connections     = $nginx::params::nx_worker_connections,
+  $worker_processes       = $nginx::params::nx_worker_processes,
+  $worker_rlimit_nofile   = $nginx::params::nx_worker_rlimit_nofile
 ) inherits nginx::params {
 
   if $caller_module_name != $module_name {
@@ -48,57 +65,57 @@ class nginx::config(
     mode  => '0644',
   }
 
-  file { $nginx::params::nx_conf_dir:
+  file { $conf_dir:
     ensure => directory,
   }
 
-  file { "${nginx::params::nx_conf_dir}/conf.d":
+  file { "${conf_dir}/conf.d":
     ensure => directory,
   }
   if $confd_purge == true {
-    File["${nginx::params::nx_conf_dir}/conf.d"] {
+    File["${conf_dir}/conf.d"] {
       purge   => true,
       recurse => true,
     }
   }
 
-  file { "${nginx::params::nx_conf_dir}/conf.mail.d":
+  file { "${conf_dir}/conf.mail.d":
     ensure => directory,
   }
   if $confd_purge == true {
-    File["${nginx::params::nx_conf_dir}/conf.mail.d"] {
+    File["${conf_dir}/conf.mail.d"] {
       purge   => true,
       recurse => true,
     }
   }
 
-  file { "${nginx::params::nx_conf_dir}/conf.d/vhost_autogen.conf":
+  file { "${conf_dir}/conf.d/vhost_autogen.conf":
     ensure => absent,
   }
 
-  file { "${nginx::params::nx_conf_dir}/conf.mail.d/vhost_autogen.conf":
+  file { "${conf_dir}/conf.mail.d/vhost_autogen.conf":
     ensure => absent,
   }
 
-  file {$nginx::config::nx_run_dir:
+  file {$run_dir:
     ensure => directory,
   }
 
-  file {$nginx::config::nx_client_body_temp_path:
+  file {$client_body_temp_path:
     ensure => directory,
-    owner  => $nginx::params::nx_daemon_user,
+    owner  => $daemon_user,
   }
 
-  file {$nginx::config::nx_proxy_temp_path:
+  file {$proxy_temp_path:
     ensure => directory,
-    owner  => $nginx::params::nx_daemon_user,
+    owner  => $daemon_user,
   }
 
-  file { "${nginx::params::nx_conf_dir}/sites-available":
+  file { "${conf_dir}/sites-available":
     ensure => directory,
   }
 
-  file { "${nginx::params::nx_conf_dir}/sites-enabled":
+  file { "${conf_dir}/sites-enabled":
     ensure => directory,
   }
 
@@ -106,24 +123,24 @@ class nginx::config(
     ensure => absent,
   }
 
-  file { "${nginx::params::nx_conf_dir}/nginx.conf":
+  file { "${conf_dir}/nginx.conf":
     ensure  => file,
     content => template('nginx/conf.d/nginx.conf.erb'),
   }
 
-  file { "${nginx::params::nx_conf_dir}/conf.d/proxy.conf":
+  file { "${conf_dir}/conf.d/proxy.conf":
     ensure  => file,
     content => template('nginx/conf.d/proxy.conf.erb'),
   }
 
-  file { "${nginx::config::nx_temp_dir}/nginx.d":
+  file { "${temp_dir}/nginx.d":
     ensure  => absent,
     purge   => true,
     recurse => true,
     force   => true,
   }
 
-  file { "${nginx::config::nx_temp_dir}/nginx.mail.d":
+  file { "${temp_dir}/nginx.mail.d":
     ensure  => absent,
     purge   => true,
     recurse => true,
