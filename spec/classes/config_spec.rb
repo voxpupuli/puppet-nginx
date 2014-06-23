65c90f199986e4c2635c7b01c9b90a742ae85174
Jan Örnstedt <jan.ornstedt@postkodlotteriet.se>
2014-06-24 01:28:21 +0200

    Added tests for pid http_tcp_nodelay http_tcp_nopush keepalive_timeout

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -213,6 +213,30 @@ describe 'nginx::config' do
             '  test2 test value 2;',
           ],
         },
+        {
+            :title => 'should set pid',
+            :attr  => 'pid',
+            :value => '/path/to/pid',
+            :match => 'pid        /path/to/pid;',
+        },
+        {
+            :title => 'should set tcp_nodelay',
+            :attr  => 'http_tcp_nodelay',
+            :value => 'on',
+            :match => '  tcp_nodelay        on;',
+        },
+        {
+            :title => 'should set tcp_nopush',
+            :attr  => 'http_tcp_nopush',
+            :value => 'on',
+            :match => '  tcp_nopush on;',
+        },
+        {
+            :title => 'should set keepalive_timeout',
+            :attr  => 'keepalive_timeout',
+            :value => '123',
+            :match => '  keepalive_timeout  123;',
+        },
       ].each do |param|
         context "when #{param[:attr]} is #{param[:value]}" do
           let :params do { param[:attr].to_sym => param[:value] } end
