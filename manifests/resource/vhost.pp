deb349047989a109da82b50e9f6b47267ecc6eea
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-08-22 16:12:48 +0000

    added ssl_session_timeout as vhost parameter

--- manifests/resource/vhost.pp
+++ manifests/resource/vhost.pp
@@ -166,6 +166,7 @@ define nginx::resource::vhost (
   $ssl_protocols          = 'SSLv3 TLSv1 TLSv1.1 TLSv1.2',
   $ssl_ciphers            = 'HIGH:!aNULL:!MD5',
   $ssl_cache              = 'shared:SSL:10m',
+  $ssl_session_timeout    = '5m',
   $ssl_stapling           = false,
   $ssl_stapling_file      = undef,
   $ssl_stapling_responder = undef,
@@ -267,6 +268,7 @@ define nginx::resource::vhost (
   validate_string($ssl_protocols)
   validate_string($ssl_ciphers)
   validate_string($ssl_cache)
+  validate_string($ssl_session_timeout)
   validate_bool($ssl_stapling)
   if ($ssl_stapling_file != undef) {
     validate_string($ssl_stapling_file)
@@ -436,7 +438,7 @@ define nginx::resource::vhost (
 
   # Add IPv6 Logic Check - Nginx service will not start if ipv6 is enabled
   # and support does not exist for it in the kernel.
-  if ($ipv6_enable == true) and (!$::ipaddress6) {
+  if ($ipv6_enable == true) and (!$ipaddress6) {
     warning('nginx: IPv6 support is not enabled or configured properly')
   }
 
