0a46f25c3cb941415a0f73ce74a4e3757192e784
Pierre Radermecker <pradermecker@cirb.irisnet.be>
2014-09-24 02:06:15 +0200

    Guard against undef

--- manifests/resource/vhost.pp
+++ manifests/resource/vhost.pp
@@ -287,7 +287,9 @@ define nginx::resource::vhost (
     validate_string($proxy)
   }
   validate_string($proxy_read_timeout)
-  validate_string($proxy_redirect)
+  if ($proxy_redirect != undef) {
+    validate_string($proxy_redirect)
+  }
   validate_array($proxy_set_header)
   if ($proxy_cache != false) {
     validate_string($proxy_cache)
