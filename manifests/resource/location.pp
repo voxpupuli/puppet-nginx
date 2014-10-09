e96bc37a10beac709123e94719115111c4d46cdf
Sven Esbjerg <esbjerg@enableit.dk>
2014-10-09 16:07:30 +0200

    Add fastcgi_index parameter.

--- manifests/resource/location.pp
+++ manifests/resource/location.pp
@@ -141,6 +141,7 @@ define nginx::resource::location (
   $proxy_connect_timeout = $nginx::config::proxy_connect_timeout,
   $proxy_set_header     = $nginx::config::proxy_set_header,
   $fastcgi              = undef,
+  $fastcgi_index        = undef,
   $fastcgi_param        = undef,
   $fastcgi_params       = "${nginx::config::conf_dir}/fastcgi_params",
   $fastcgi_script       = undef,
