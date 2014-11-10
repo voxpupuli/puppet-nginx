2bea3e7600abebbee08c0fa736fbd20153201898
Matthew Haughton <3flex@users.noreply.github.com>
2014-11-10 14:13:27 -0500

    spec_helper: remove module-data-backend dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'fixtures/modules/module_data/lib/hiera/backend/module_data_backend.rb'
 
 RSpec.configure do |c|
-  c.hiera_config = File.join('spec', 'fixtures', 'hiera', 'hiera.yaml')
-
   c.default_facts = {
     :kernel          => 'Linux',
     :concat_basedir  => '/var/lib/puppet/concat',
