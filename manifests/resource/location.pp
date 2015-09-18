c95d57a5b102deb52b7a6a7bbb977b8b44b48102
Erik Anderson <edawebdesign@gmail.com>
2015-09-18 12:32:29 -0600

    Convert priority to integer
    Ensure that priority is an integer before comparison

--- manifests/resource/location.pp
+++ manifests/resource/location.pp
@@ -308,7 +308,7 @@ define nginx::resource::location (
     fail('$priority must be an integer.')
   }
   validate_array($rewrite_rules)
-  if ($priority < 401) or ($priority > 899) {
+  if (($priority + 0) < 401) or (($priority + 0) > 899) {
     fail('$priority must be in the range 401-899.')
   }
 
