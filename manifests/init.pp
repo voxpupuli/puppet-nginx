4c97080fdc2c6f9f5e578f96759b76d434cce1de
Erik Anderson <erik.anderson@mx.com>
2015-11-11 16:12:45 -0700

    Provide more info on deprecations for user

--- manifests/init.pp
+++ manifests/init.pp
@@ -140,71 +140,85 @@ class nginx (
   ### This block makes me sad, but what can you do.... we need to do this
   ### migration the Right Way(tm) -- JDF
 
-  if $client_body_buffer_size or
-        $client_body_temp_path or
-        $client_max_body_size or
-        $confd_purge or
-        $conf_dir or
-        $conf_template or
-        $daemon_user or
-        $events_use or
-        $fastcgi_cache_inactive or
-        $fastcgi_cache_key or
-        $fastcgi_cache_keys_zone or
-        $fastcgi_cache_levels or
-        $fastcgi_cache_max_size or
-        $fastcgi_cache_path or
-        $fastcgi_cache_use_stale or
-        $gzip or
-        $http_access_log or
-        $http_cfg_append or
-        $http_tcp_nodelay or
-        $http_tcp_nopush or
-        $keepalive_timeout or
-        $logdir or
-        $log_format or
-        $mail or
-        $multi_accept or
-        $names_hash_bucket_size or
-        $names_hash_max_size or
-        $nginx_error_log or
-        $pid or
-        $proxy_buffers or
-        $proxy_buffer_size or
-        $proxy_cache_inactive or
-        $proxy_cache_keys_zone or
-        $proxy_cache_levels or
-        $proxy_cache_max_size or
-        $proxy_cache_path or
-        $proxy_conf_template or
-        $proxy_connect_timeout or
-        $proxy_headers_hash_bucket_size or
-        $proxy_http_version or
-        $proxy_read_timeout or
-        $proxy_redirect or
-        $proxy_send_timeout or
-        $proxy_set_header or
-        $proxy_temp_path or
-        $run_dir or
-        $sendfile or
-        $server_tokens or
-        $spdy or
-        $super_user or
-        $temp_dir or
-        $types_hash_bucket_size or
-        $types_hash_max_size or
-        $vhost_purge or
-        $worker_connections or
-        $worker_processes or
-        $worker_rlimit_nofile or
-        $global_owner or
-        $global_group or
-        $global_mode or
-        $sites_available_owner or
-        $sites_available_group or
-        $sites_available_mode {
-          include ::nginx::notice::config
-        }
+  {
+    client_body_buffer_size => $client_body_buffer_size,
+    client_body_temp_path => $client_body_temp_path,
+    client_max_body_size => $client_max_body_size,
+    confd_purge => $confd_purge,
+    conf_dir => $conf_dir,
+    conf_template => $conf_template,
+    daemon_user => $daemon_user,
+    events_use => $events_use,
+    fastcgi_cache_inactive => $fastcgi_cache_inactive,
+    fastcgi_cache_key => $fastcgi_cache_key,
+    fastcgi_cache_keys_zone => $fastcgi_cache_keys_zone,
+    fastcgi_cache_levels => $fastcgi_cache_levels,
+    fastcgi_cache_max_size => $fastcgi_cache_max_size,
+    fastcgi_cache_path => $fastcgi_cache_path,
+    fastcgi_cache_use_stale => $fastcgi_cache_use_stale,
+    gzip => $gzip,
+    http_access_log => $http_access_log,
+    http_cfg_append => $http_cfg_append,
+    http_tcp_nodelay => $http_tcp_nodelay,
+    http_tcp_nopush => $http_tcp_nopush,
+    keepalive_timeout => $keepalive_timeout,
+    logdir => $logdir,
+    log_format => $log_format,
+    mail => $mail,
+    multi_accept => $multi_accept,
+    names_hash_bucket_size => $names_hash_bucket_size,
+    names_hash_max_size => $names_hash_max_size,
+    nginx_error_log => $nginx_error_log,
+    pid => $pid,
+    proxy_buffers => $proxy_buffers,
+    proxy_buffer_size => $proxy_buffer_size,
+    proxy_cache_inactive => $proxy_cache_inactive,
+    proxy_cache_keys_zone => $proxy_cache_keys_zone,
+    proxy_cache_levels => $proxy_cache_levels,
+    proxy_cache_max_size => $proxy_cache_max_size,
+    proxy_cache_path => $proxy_cache_path,
+    proxy_conf_template => $proxy_conf_template,
+    proxy_connect_timeout => $proxy_connect_timeout,
+    proxy_headers_hash_bucket_size => $proxy_headers_hash_bucket_size,
+    proxy_http_version => $proxy_http_version,
+    proxy_read_timeout => $proxy_read_timeout,
+    proxy_redirect => $proxy_redirect,
+    proxy_send_timeout => $proxy_send_timeout,
+    proxy_set_header => $proxy_set_header,
+    proxy_temp_path => $proxy_temp_path,
+    run_dir => $run_dir,
+    sendfile => $sendfile,
+    server_tokens => $server_tokens,
+    spdy => $spdy,
+    super_user => $super_user,
+    temp_dir => $temp_dir,
+    types_hash_bucket_size => $types_hash_bucket_size,
+    types_hash_max_size => $types_hash_max_size,
+    vhost_purge => $vhost_purge,
+    worker_connections => $worker_connections,
+    worker_processes => $worker_processes,
+    worker_rlimit_nofile => $worker_rlimit_nofile,
+    global_owner => $global_owner,
+    global_group => $global_group,
+    global_mode => $global_mode,
+    sites_available_owner => $sites_available_owner,
+    sites_available_group => $sites_available_group,
+    sites_available_mod => $sites_available_mod,
+  }.each | $key, $val |{
+    if ($val){
+      $message = @("MESSAGE")
+        [nginx] *** DEPRECATION WARNING***: HI! I notice that you are declaring some attributes in Class[nginx]:
+        **attribute: $key
+        **value: $val
+        It is highly recommended to set these values via Hiera going forward. This will become mandatory in the near future.
+        Please check out https://github.com/jfryman/puppet-nginx/blob/master/docs/hiera.md for more information.
+        ------------------------------------------------------------------------
+
+        | MESSAGE
+
+      notify { $message: }
+    }
+  }
 
   ### END DEPRECATION WARNING ###
 
