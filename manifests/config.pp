527f6989b60fe47a02ce487fedf16a7840ab0eea
Matthew Robinson <matthew.robinson@ensighten.com>
2016-01-25 19:41:06 -0800

    Add support for managing events.accept_mutex settings

--- manifests/config.pp
+++ manifests/config.pp
@@ -43,6 +43,8 @@ class nginx::config(
   ### END Module/App Configuration ###
 
   ### START Nginx Configuration ###
+  $accept_mutex                   = 'on',
+  $accept_mutex_delay             = '500ms',
   $client_body_buffer_size        = '128k',
   $client_max_body_size           = '10m',
   $events_use                     = false,
