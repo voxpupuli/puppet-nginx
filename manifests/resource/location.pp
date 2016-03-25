1044490036892698ee046d7256fc23112e98fcc8
herwig Bogaert <herwig.bogaert@viaa.be>
2016-03-25 11:14:29 +0000

    Allow multiple 'proxy_cache_valid' directives

--- manifests/resource/location.pp
+++ manifests/resource/location.pp
@@ -296,7 +296,7 @@ define nginx::resource::location (
     validate_string($proxy_cache_use_stale)
   }
   if ($proxy_cache_valid != false) {
-    validate_string($proxy_cache_valid)
+    validate_slength(any2array($proxy_cache_valid),12,1)
   }
   if ($proxy_method != undef) {
     validate_string($proxy_method)
