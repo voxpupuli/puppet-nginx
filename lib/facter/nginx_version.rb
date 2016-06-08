19ef50cb3d9bb5ff3ab669d9a54dc4af19e52fba
Tim Meusel <tim@bastelfreak.de>
2016-06-08 12:01:17 +0200

    make fact work on old nginx as well
    CentOS 7 (default fact works): nginx version: nginx/1.10.0
    CentOS 6: nginx: nginx version: nginx/1.0.0

--- lib/facter/nginx_version.rb
+++ lib/facter/nginx_version.rb
@@ -2,7 +2,7 @@ Facter.add(:nginx_version) do
   setcode do
     if Facter.value('kernel') != 'windows' && Facter::Util::Resolution.which('nginx')
       nginx_version = Facter::Util::Resolution.exec('nginx -v 2>&1')
-      %r{^nginx version: nginx\/([\w\.]+)}.match(nginx_version)[1]
+      %r{nginx version: nginx\/([\w\.]+)}.match(nginx_version)[1]
     end
   end
 end
