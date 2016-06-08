1653af5608d5599e0492f84ae9f8e15604a8ce8f
Greg Koch <greg@ekingme.com>
2016-06-08 14:33:02 -0400

    update nginx_version fact to check for kernel not equal windows

--- lib/facter/nginx_version.rb
+++ lib/facter/nginx_version.rb
@@ -1,6 +1,6 @@
 Facter.add(:nginx_version) do
   setcode do
-    if Facter::Util::Resolution.which('nginx')
+    if Facter.value('kernel') != 'windows' && Facter::Util::Resolution.which('nginx')
       nginx_version = Facter::Util::Resolution.exec('nginx -v 2>&1')
       %r{^nginx version: nginx\/([\w\.]+)}.match(nginx_version)[1]
     end
