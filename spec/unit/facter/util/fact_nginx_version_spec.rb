78ca1d0ca8440eb44b67aba92434e46b73aef0a4
William Yardley <wby@axs.com>
2016-10-08 22:06:10 -0700

    add test for openresty nginx version fact (#890, #908)

--- spec/unit/facter/util/fact_nginx_version_spec.rb
+++ spec/unit/facter/util/fact_nginx_version_spec.rb
@@ -17,4 +17,11 @@ describe Facter::Util::Fact do
     end
     it { expect(Facter.fact(:nginx_version).value).to eq('0.7.0') }
   end
+  context 'with openresty nginx' do
+    before do
+      Facter::Util::Resolution.stubs(:which).with('nginx').returns(true)
+      Facter::Util::Resolution.stubs(:exec).with('nginx -v 2>&1').returns('nginx: nginx version: openresty/1.11.2.1')
+    end
+    it { expect(Facter.fact(:nginx_version).value).to eq('1.11.2.1') }
+  end
 end
