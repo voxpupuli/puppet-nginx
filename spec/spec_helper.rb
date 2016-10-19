0221f289a677fe2e9aaf2ad09604b93a0937b15f
Dennis Hoppe <github@debian-solutions.de>
2016-10-19 22:02:53 +0200

    Configure mock with rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ RSpec.configure do |c|
   default_facts.merge!(YAML.load(File.read(File.expand_path('../default_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_facts.yml', __FILE__))
   default_facts.merge!(YAML.load(File.read(File.expand_path('../default_module_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_module_facts.yml', __FILE__))
   c.default_facts = default_facts
+  c.mock_with :rspec
 end
 
 # vim: syntax=ruby
