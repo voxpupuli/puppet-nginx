7a88a112c4968a0e38a4ecc7ff1e7d6878484004
Dominic <dev@snowgarden.ch>
2016-11-07 02:59:53 +0100

    Adds new SSL && protocol specific directives to mailhost setup (#769)
    * Adds new SSL && protocol specific directives to mailhost setup
    
    The SSL setup customisation of a mailhost is very limited. To allow
    more directives to be set, the ssl configuration was extracted and
    arranged like the setup for a vhost.
    The setup was also expanded to customize protocol specific authentication
    method and capabilities.
    
    * Extended test cases to include newly introduced ssl and protocol specific options in mailhost setup
    
    * support raw_prepend, raw_append, mailhost_cfg_append, mailhost_cfg_prepend, in mailhost
    
    * Fixes rubocop issues
    
    * Render template without a new line
    
    * Fixes typo in mailhost.pp
    
    * Fix inlining after merge and dropping newlines when rendering a template

--- spec/defines/resource_mailhost_spec.rb
+++ spec/defines/resource_mailhost_spec.rb
@@ -49,31 +49,31 @@ describe 'nginx::resource::mailhost' do
           title: 'should enable IPv6',
           attr: 'ipv6_enable',
           value: true,
-          match: '  listen [::]:80 default ipv6only=on;'
+          match: '  listen                [::]:80 default ipv6only=on;'
         },
         {
           title: 'should not enable IPv6',
           attr: 'ipv6_enable',
           value: false,
-          notmatch: %r{  listen \[::\]:80 default ipv6only=on;}
+          notmatch: %r{  listen                \[::\]:80 default ipv6only=on;}
         },
         {
           title: 'should set the IPv6 listen IP',
           attr: 'ipv6_listen_ip',
           value: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
-          match: '  listen [2001:0db8:85a3:0000:0000:8a2e:0370:7334]:80 default ipv6only=on;'
+          match: '  listen                [2001:0db8:85a3:0000:0000:8a2e:0370:7334]:80 default ipv6only=on;'
         },
         {
           title: 'should set the IPv6 listen port',
           attr: 'ipv6_listen_port',
           value: 45,
-          match: '  listen [::]:45 default ipv6only=on;'
+          match: '  listen                [::]:45 default ipv6only=on;'
         },
         {
           title: 'should set the IPv6 listen options',
           attr: 'ipv6_listen_options',
           value: 'spdy',
-          match: '  listen [::]:80 spdy;'
+          match: '  listen                [::]:80 spdy;'
         },
         {
           title: 'should set servername(s)',
@@ -122,6 +122,48 @@ describe 'nginx::resource::mailhost' do
           attr: 'starttls',
           value: 'off',
           notmatch: %r{  ssl_session_timeout  5m;}
+        },
+        {
+          title: 'should contain raw_prepend directives',
+          attr: 'raw_prepend',
+          value: [
+            'if (a) {',
+            '  b;',
+            '}'
+          ],
+          match: %r{^\s+if \(a\) \{\n\s++b;\n\s+\}}
+        },
+        {
+          title: 'should contain raw_append directives',
+          attr: 'raw_append',
+          value: [
+            'if (a) {',
+            '  b;',
+            '}'
+          ],
+          match: %r{^\s+if \(a\) \{\n\s++b;\n\s+\}}
+        },
+        {
+          title: 'should contain ordered prepended directives',
+          attr: 'mailhost_cfg_prepend',
+          value: { 'test1' => 'test value 1', 'test2' => ['test value 2a', 'test value 2b'], 'test3' => 'test value 3' },
+          match: [
+            '  test1 test value 1;',
+            '  test2 test value 2a;',
+            '  test2 test value 2b;',
+            '  test3 test value 3;'
+          ]
+        },
+        {
+          title: 'should contain ordered appended directives',
+          attr: 'mailhost_cfg_append',
+          value: { 'test1' => 'test value 1', 'test2' => ['test value 2a', 'test value 2b'], 'test3' => 'test value 3' },
+          match: [
+            '  test1 test value 1;',
+            '  test2 test value 2a;',
+            '  test2 test value 2b;',
+            '  test3 test value 3;'
+          ]
         }
       ].each do |param|
         context "when #{param[:attr]} is #{param[:value]}" do
@@ -137,10 +179,140 @@ describe 'nginx::resource::mailhost' do
 
           it { is_expected.to contain_concat__fragment("#{title}-header") }
           it param[:title] do
-            lines = catalogue.resource('concat::fragment', "#{title}-header").send(:parameters)[:content].split("\n")
-            expect(lines & Array(param[:match])).to eq(Array(param[:match]))
-            Array(param[:notmatch]).each do |item|
-              is_expected.to contain_concat__fragment("#{title}-header").without_content(item)
+            matches = Array(param[:match])
+
+            if matches.all? { |m| m.is_a? Regexp }
+              matches.each { |item| is_expected.to contain_concat__fragment("#{title}-header").with_content(item) }
+            else
+              lines = catalogue.resource('concat::fragment', "#{title}-header").send(:parameters)[:content].split("\n")
+              expect(lines & Array(param[:match])).to eq(Array(param[:match]))
+            end
+          end
+        end
+      end
+    end
+
+    describe 'mailhost template content for imap' do
+      [
+        {
+          title: 'should set imap_auth',
+          attr: 'imap_auth',
+          value: 'login',
+          match: '  imap_auth           login;'
+        },
+        {
+          title: 'should set imap_capabilities',
+          attr: 'imap_capabilities',
+          value: ['"SIZE 52428800"', 'IMAP4rev1', 'UIDPLUS'],
+          match: '  imap_capabilities   "SIZE 52428800" IMAP4rev1 UIDPLUS;'
+        },
+        {
+          title: 'should set imap_client_buffer',
+          attr: 'imap_client_buffer',
+          value: '8k',
+          match: '  imap_client_buffer  8k;'
+        }
+      ].each do |param|
+        context "when #{param[:attr]} is #{param[:value]}" do
+          let :default_params do
+            {
+              listen_port: 25,
+              ipv6_enable: true,
+              protocol: 'imap'
+            }
+          end
+
+          let(:params) { default_params.merge(param[:attr].to_sym => param[:value]) }
+
+          it { is_expected.to contain_concat__fragment("#{title}-header") }
+          it param[:title] do
+            matches = Array(param[:match])
+
+            if matches.all? { |m| m.is_a? Regexp }
+              matches.each { |item| is_expected.to contain_concat__fragment("#{title}-header").with_content(item) }
+            else
+              lines = catalogue.resource('concat::fragment', "#{title}-header").send(:parameters)[:content].split("\n")
+              expect(lines & Array(param[:match])).to eq(Array(param[:match]))
+            end
+          end
+        end
+      end
+    end
+
+    describe 'mailhost template content for pop3' do
+      [
+        {
+          title: 'should set pop3_auth',
+          attr: 'pop3_auth',
+          value: 'login',
+          match: '  pop3_auth          login;'
+        },
+        {
+          title: 'should set pop3_capabilities',
+          attr: 'pop3_capabilities',
+          value: %w(TOP USER UIDL),
+          match: '  pop3_capabilities  TOP USER UIDL;'
+        }
+      ].each do |param|
+        context "when #{param[:attr]} is #{param[:value]}" do
+          let :default_params do
+            {
+              listen_port: 25,
+              ipv6_enable: true,
+              protocol: 'pop3'
+            }
+          end
+          let(:params) { default_params.merge(param[:attr].to_sym => param[:value]) }
+
+          it { is_expected.to contain_concat__fragment("#{title}-header") }
+          it param[:title] do
+            matches = Array(param[:match])
+
+            if matches.all? { |m| m.is_a? Regexp }
+              matches.each { |item| is_expected.to contain_concat__fragment("#{title}-header").with_content(item) }
+            else
+              lines = catalogue.resource('concat::fragment', "#{title}-header").send(:parameters)[:content].split("\n")
+              expect(lines & Array(param[:match])).to eq(Array(param[:match]))
+            end
+          end
+        end
+      end
+    end
+
+    describe 'mailhost template content for smtp' do
+      [
+        {
+          title: 'should set smtp_auth',
+          attr: 'smtp_auth',
+          value: 'login',
+          match: '  smtp_auth          login;'
+        },
+        {
+          title: 'should set smtp_capabilities',
+          attr: 'smtp_capabilities',
+          value: %w(8BITMIME PIPELINING HELP),
+          match: '  smtp_capabilities  8BITMIME PIPELINING HELP;'
+        }
+      ].each do |param|
+        context "when #{param[:attr]} is #{param[:value]}" do
+          let :default_params do
+            {
+              listen_port: 25,
+              ipv6_enable: true,
+              protocol: 'smtp'
+            }
+          end
+          let(:params) { default_params.merge(param[:attr].to_sym => param[:value]) }
+
+          it { is_expected.to contain_concat__fragment("#{title}-header") }
+          it param[:title] do
+            matches = Array(param[:match])
+
+            if matches.all? { |m| m.is_a? Regexp }
+              matches.each { |item| is_expected.to contain_concat__fragment("#{title}-header").with_content(item) }
+            else
+              lines = catalogue.resource('concat::fragment', "#{title}-header").send(:parameters)[:content].split("\n")
+              expect(lines & Array(param[:match])).to eq(Array(param[:match]))
             end
           end
         end
@@ -150,16 +322,16 @@ describe 'nginx::resource::mailhost' do
     describe 'mailhost template content (SSL enabled)' do
       [
         {
-          title: 'should enable SSL',
+          title: 'should set starttls',
           attr: 'starttls',
           value: 'on',
-          match: '  ssl_session_timeout  5m;'
+          match: '  starttls              on;'
         },
         {
-          title: 'should enable SSL',
+          title: 'should set starttls',
           attr: 'starttls',
           value: 'only',
-          match: '  ssl_session_timeout  5m;'
+          match: '  starttls              only;'
         },
         {
           title: 'should not enable SSL',
@@ -171,13 +343,97 @@ describe 'nginx::resource::mailhost' do
           title: 'should set ssl_certificate',
           attr: 'ssl_cert',
           value: 'test-ssl-cert',
-          match: '  ssl_certificate      test-ssl-cert;'
+          match: '  ssl_certificate            test-ssl-cert;'
         },
         {
           title: 'should set ssl_certificate_key',
           attr: 'ssl_key',
           value: 'test-ssl-cert-key',
-          match: '  ssl_certificate_key  test-ssl-cert-key;'
+          match: '  ssl_certificate_key        test-ssl-cert-key;'
+        },
+        {
+          title: 'should set ssl_ciphers',
+          attr: 'ssl_ciphers',
+          value: 'ECDHE-ECDSA-CHACHA20-POLY1305',
+          match: '  ssl_ciphers                ECDHE-ECDSA-CHACHA20-POLY1305;'
+        },
+        {
+          title: 'should set ssl_client_certificate',
+          attr: 'ssl_client_cert',
+          value: 'client-cert',
+          match: '  ssl_client_certificate     client-cert;'
+        },
+        {
+          title: 'should set ssl_crl',
+          attr: 'ssl_crl',
+          value: 'crl-file',
+          match: '  ssl_crl                    crl-file;'
+        },
+        {
+          title: 'should set ssl_dhparam',
+          attr: 'ssl_dhparam',
+          value: 'dhparam-file',
+          match: '  ssl_dhparam                dhparam-file;'
+        },
+        {
+          title: 'should set ssl_ecdh_curve',
+          attr: 'ssl_ecdh_curve',
+          value: 'secp521r1',
+          match: '  ssl_ecdh_curve             secp521r1;'
+        },
+        {
+          title: 'should set ssl_client_certificate',
+          attr: 'ssl_client_cert',
+          value: 'client-cert',
+          match: '  ssl_client_certificate     client-cert;'
+        },
+        {
+          title: 'should set ssl_password_file',
+          attr: 'ssl_password_file',
+          value: 'password-file',
+          match: '  ssl_password_file          password-file;'
+        },
+        {
+          title: 'should set ssl_protocols',
+          attr: 'ssl_protocols',
+          value: 'TLSv1.2',
+          match: '  ssl_protocols              TLSv1.2;'
+        },
+        {
+          title: 'should set ssl_session_cache',
+          attr: 'ssl_session_cache',
+          value: 'none',
+          match: '  ssl_session_cache          none;'
+        },
+        {
+          title: 'should set ssl_session_ticket_key',
+          attr: 'ssl_session_ticket_key',
+          value: 'key-file',
+          match: '  ssl_session_ticket_key     key-file;'
+        },
+        {
+          title: 'should set ssl_session_tickets',
+          attr: 'ssl_session_tickets',
+          value: 'on',
+          match: '  ssl_session_tickets        on;'
+        },
+        {
+          title: 'should set ssl_session_timeout',
+          attr: 'ssl_session_timeout',
+          value: '20m',
+          match: '  ssl_session_timeout        20m;'
+        },
+        {
+          title: 'should set ssl_trusted_certificate',
+          attr: 'ssl_trusted_cert',
+          value: 'trust-cert',
+          match: '  ssl_trusted_certificate    trust-cert;'
+        },
+        {
+          title: 'should set ssl_verify_depth',
+          attr: 'ssl_verify_depth',
+          value: 2,
+          match: '  ssl_verify_depth           2;'
         }
       ].each do |param|
         context "when #{param[:attr]} is #{param[:value]}" do
@@ -193,10 +449,13 @@ describe 'nginx::resource::mailhost' do
 
           it { is_expected.to contain_concat__fragment("#{title}-header") }
           it param[:title] do
-            lines = catalogue.resource('concat::fragment', "#{title}-header").send(:parameters)[:content].split("\n")
-            expect(lines & Array(param[:match])).to eq(Array(param[:match]))
-            Array(param[:notmatch]).each do |item|
-              is_expected.to contain_concat__fragment("#{title}-header").without_content(item)
+            matches = Array(param[:match])
+
+            if matches.all? { |m| m.is_a? Regexp }
+              matches.each { |item| is_expected.to contain_concat__fragment("#{title}-header").with_content(item) }
+            else
+              lines = catalogue.resource('concat::fragment', "#{title}-header").send(:parameters)[:content].split("\n")
+              expect(lines & Array(param[:match])).to eq(Array(param[:match]))
             end
           end
         end
@@ -208,38 +467,38 @@ describe 'nginx::resource::mailhost' do
         {
           title: 'should set the IPv4 SSL listen port',
           attr: 'ssl_port',
-          value: '45',
-          match: '  listen       *:45;'
+          value: 45,
+          match: '  listen                *:45;'
         },
         {
           title: 'should enable IPv6',
           attr: 'ipv6_enable',
           value: true,
-          match: '  listen [::]:587 default ipv6only=on;'
+          match: '  listen                [::]:587 default ipv6only=on;'
         },
         {
           title: 'should not enable IPv6',
           attr: 'ipv6_enable',
           value: false,
-          notmatch: %r{  listen \[::\]:587 default ipv6only=on;}
+          notmatch: %r{  listen\s+\[::\]:587 default ipv6only=on;}
         },
         {
           title: 'should set the IPv6 listen IP',
           attr: 'ipv6_listen_ip',
           value: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
-          match: '  listen [2001:0db8:85a3:0000:0000:8a2e:0370:7334]:587 default ipv6only=on;'
+          match: '  listen                [2001:0db8:85a3:0000:0000:8a2e:0370:7334]:587 default ipv6only=on;'
         },
         {
           title: 'should set the IPv6 ssl port',
           attr: 'ssl_port',
           value: 45,
-          match: '  listen [::]:45 default ipv6only=on;'
+          match: '  listen                [::]:45 default ipv6only=on;'
         },
         {
           title: 'should set the IPv6 listen options',
           attr: 'ipv6_listen_options',
           value: 'spdy',
-          match: '  listen [::]:587 spdy;'
+          match: '  listen                [::]:587 spdy;'
         },
         {
           title: 'should set servername(s)',
@@ -275,25 +534,25 @@ describe 'nginx::resource::mailhost' do
           title: 'should set ssl_protocols',
           attr: 'ssl_protocols',
           value: 'test-ssl-protocol',
-          match: '  ssl_protocols test-ssl-protocol;'
+          match: '  ssl_protocols              test-ssl-protocol;'
         },
         {
           title: 'should set ssl_ciphers',
           attr: 'ssl_ciphers',
           value: 'test-ssl-ciphers',
-          match: '  ssl_ciphers test-ssl-ciphers;'
+          match: '  ssl_ciphers                test-ssl-ciphers;'
         },
         {
           title: 'should set ssl_certificate',
           attr: 'ssl_cert',
           value: 'test-ssl-cert',
-          match: '  ssl_certificate      test-ssl-cert;'
+          match: '  ssl_certificate            test-ssl-cert;'
         },
         {
           title: 'should set ssl_certificate_key',
           attr: 'ssl_key',
           value: 'test-ssl-cert-key',
-          match: '  ssl_certificate_key  test-ssl-cert-key;'
+          match: '  ssl_certificate_key        test-ssl-cert-key;'
         }
       ].each do |param|
         context "when #{param[:attr]} is #{param[:value]}" do
@@ -313,10 +572,13 @@ describe 'nginx::resource::mailhost' do
 
           it { is_expected.to contain_concat__fragment("#{title}-ssl") }
           it param[:title] do
-            lines = catalogue.resource('concat::fragment', "#{title}-ssl").send(:parameters)[:content].split("\n")
-            expect(lines & Array(param[:match])).to eq(Array(param[:match]))
-            Array(param[:notmatch]).each do |item|
-              is_expected.to contain_concat__fragment("#{title}-ssl").without_content(item)
+            matches = Array(param[:match])
+
+            if matches.all? { |m| m.is_a? Regexp }
+              matches.each { |item| is_expected.to contain_concat__fragment("#{title}-ssl").with_content(item) }
+            else
+              lines = catalogue.resource('concat::fragment', "#{title}-ssl").send(:parameters)[:content].split("\n")
+              expect(lines & Array(param[:match])).to eq(Array(param[:match]))
             end
           end
         end
