9fde1cf9a8dc0a4d0d3dc3323384309226d8fa05
François Boukhalfa <francois@ceonizme.fr>
2017-11-16 14:03:57 +0100

    enhance logging support per location: adding access_log & log_not_found directives

--- manifests/resource/location.pp
+++ manifests/resource/location.pp
@@ -212,6 +212,8 @@ define nginx::resource::location (
   Boolean $mp4                                        = false,
   Boolean $flv                                        = false,
   Optional[String] $expires                           = undef,
+  Optional[Enum['on','off']] $log_not_found           = undef,
+  Optional[Enum['on','off']] $access_log              = undef,
 ) {
 
   if ! defined(Class['nginx']) {
