9bbec6cef67e33464e6324faf839d8d117ea0574
fnoop <fnoopdogg@gmail.com>
2017-11-20 09:20:35 +0000

    Add datatype to new repo_release parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -145,7 +145,7 @@ class nginx (
   $package_source                                            = 'nginx',
   $package_flavor                                            = undef,
   $manage_repo                                               = $::nginx::params::manage_repo,
-  $repo_release                                              = undef,
+  Optional[String] $repo_release                             = undef,
   $passenger_package_ensure                                  = 'present',
   ### END Package Configuration ###
 
