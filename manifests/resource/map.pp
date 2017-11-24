f61c29476c19b21ec21806ec792327d4effb66f4
Alex Siryi <desertkun@gmail.com>
2017-11-25 00:01:16 +0200

    Added advanced nginx mapping support
    So map key/values can be defined in different places

--- manifests/resource/map.pp
+++ manifests/resource/map.pp
@@ -7,7 +7,10 @@
 #   [*default*]    - Sets the resulting value if the source values fails to
 #                    match any of the variants.
 #   [*string*]     - Source string or variable to provide mapping for
-#   [*mappings*]   - Hash of map lookup keys and resultant values
+#   [*mappings*]   - Hash of map lookup keys and resultant values. If this value is defined,
+#                    then Puppet will use it all to generate apropriate map. If this value is not defined,
+#                    then Puppet would expect nginx::resource::map::entry definitions to construct the map
+#                    from different places. See Advanced Usage for example.
 #   [*hostnames*]  - Indicates that source values can be hostnames with a
 #                    prefix or suffix mask.
 #   [*include_files*]   - An array of external files to include
@@ -16,7 +19,7 @@
 #
 # Requires:
 #
-# Sample Usage:
+# Sample Usage (with mappings defined):
 #
 #  nginx::resource::map { 'backend_pool':
 #    ensure    => present,
@@ -27,6 +30,27 @@
 #      '*.nyc.example.com' => 'ny-pool-1',
 #      '*.sf.example.com'  => 'sf-pool-1',
 #    }
+#  }
+#
+# Advanced Usage (with external entry definitions):
+#
+#  nginx::resource::map { 'backend_pool':
+#    ensure    => present,
+#    hostnames => true,
+#    default   => 'ny-pool-1,
+#    string    => '$http_host'
+#  }
+#
+#  nginx::resource::map::entry { 'backend_pool_nyc':
+#    map => 'backend_pool',
+#    key => '*.nyc.example.com',
+#    value => 'ny-pool-1'
+#  }
+#
+#  nginx::resource::map::entry { 'backend_pool_sf':
+#    map => 'backend_pool',
+#    key => '*.sf.example.com',
+#    value => 'sf-pool-1'
 #  }
 #
 # Sample Usage (preserving input of order of mappings):
@@ -46,6 +70,29 @@
 #    include_files => [ '/etc/nginx/conf.d/redirections.map']
 #
 # }
+#
+# Advanced Usage (preserving input of order of mappings):
+#
+#  nginx::resource::map { 'backend_pool':
+#    ensure    => present,
+#    hostnames => true,
+#    default   => 'ny-pool-1,
+#    string    => '$http_host'
+#  }
+#
+#  nginx::resource::map::entry { 'backend_pool_nyc':
+#    map => 'backend_pool',
+#    key => '*.nyc.example.com',
+#    value => 'ny-pool-1',
+#    order => '2'
+#  }
+#
+#  nginx::resource::map::entry { 'backend_pool_sf':
+#    map => 'backend_pool',
+#    key => '*.sf.example.com',
+#    value => 'sf-pool-1',
+#    order => '1'
+#  }
 #
 # Sample Hiera usage:
 #
@@ -72,12 +119,12 @@
 
 
 define nginx::resource::map (
-  String[2] $string,
-  Variant[Array, Hash] $mappings,
-  Optional[String] $default         = undef,
-  Enum['absent', 'present'] $ensure = 'present',
-  Array[String] $include_files      = [],
-  Boolean $hostnames                = false
+  String $string,
+  Variant[Array, Hash, Undef] $mappings = undef,
+  Optional[String] $default             = undef,
+  Enum['absent', 'present'] $ensure     = 'present',
+  Array[String] $include_files          = [],
+  Boolean $hostnames                    = false
 ) {
   if ! defined(Class['nginx']) {
     fail('You must include the nginx base class before using any defined resources')
@@ -86,21 +133,54 @@ define nginx::resource::map (
   $root_group = $::nginx::root_group
   $conf_dir   = "${::nginx::conf_dir}/conf.d"
 
-  $ensure_real = $ensure ? {
-    'absent' => absent,
-    default  => 'file',
-  }
+  if ($mappings) {
+    # if mappings are provided, use it completely to generate the conf file
+    $ensure_real = $ensure ? {
+      'absent' => absent,
+      default  => 'file',
+    }
 
-  File {
-    owner => 'root',
-    group => $root_group,
-    mode  => '0644',
-  }
+    File {
+      owner => 'root',
+      group => $root_group,
+      mode  => '0644',
+    }
 
-  file { "${::nginx::conf_dir}/conf.d/${name}-map.conf":
-    ensure  => $ensure_real,
-    content => template('nginx/conf.d/map.erb'),
-    notify  => Class['::nginx::service'],
-    require => File[$conf_dir],
+    file { "${::nginx::conf_dir}/conf.d/${name}-map.conf":
+      ensure  => $ensure_real,
+      content => template('nginx/conf.d/map_hash.erb'),
+      notify  => Class['::nginx::service'],
+      require => File[$conf_dir],
+    }
+  } else {
+    # otherwise, expect nginx::resource::map::entry resources to collect it from fragments
+    $ensure_real = $ensure ? {
+      'absent' => absent,
+      default  => present,
+    }
+
+    File {
+      owner => 'root',
+      group => $root_group, 
+      mode  => '0644',
+    }
+
+    concat { "${::nginx::conf_dir}/conf.d/${name}-map.conf":
+      ensure  => $ensure_real,
+      notify  => Class['::nginx::service'],
+      require => File[$conf_dir],
+    }
+
+    concat::fragment { "${::nginx::conf_dir}/conf.d/${name}-header":
+      target => "${::nginx::conf_dir}/conf.d/${name}-map.conf",
+      content => template('nginx/conf.d/map/header.erb'),
+      order => '0'
+    }
+
+    concat::fragment { "${::nginx::conf_dir}/conf.d/${name}-footer":
+      target => "${::nginx::conf_dir}/conf.d/${name}-map.conf",
+      content => template('nginx/conf.d/map/footer.erb'),
+      order => '9'
+    }
   }
 }
---
+++ manifests/resource/map/entry.pp
@@ -0,0 +1,82 @@
+# define: nginx::resource::map::entry
+#
+# This definition creates a new mapping entry key => value pair for NGINX. It is used in cases when
+# $mappings variable is not defined. If a resource nginx::resource::map with no $mappings is defined,
+# Puppet would expect nginx::resource::map::entry definitions to construct the map from different places.
+#
+#   [*map*]        - A title of the corresponding nginx::resource::map resource.
+#   [*key*]        - A key for the mapping
+#   [*value*]      - A value for the mapping
+#   [*order*]      - Optional order for other mapping entries. Default is the key itself.
+#
+# Actions:
+#
+# Requires:
+#
+# Usage:
+#
+#  nginx::resource::map { 'backend_pool':
+#    ensure    => present,
+#    hostnames => true,
+#    default   => 'ny-pool-1,
+#    string    => '$http_host'
+#  }
+#
+#  nginx::resource::map::entry { 'backend_pool_nyc':
+#    map => 'backend_pool',
+#    key => '*.nyc.example.com',
+#    value => 'ny-pool-1'
+#  }
+#
+#  nginx::resource::map::entry { 'backend_pool_sf':
+#    map => 'backend_pool',
+#    key => '*.sf.example.com',
+#    value => 'sf-pool-1'
+#  }
+#
+# Usage (preserving input of order of mappings):
+#
+#  nginx::resource::map { 'backend_pool':
+#    ensure    => present,
+#    hostnames => true,
+#    default   => 'ny-pool-1,
+#    string    => '$http_host'
+#  }
+#
+#  nginx::resource::map::entry { 'backend_pool_nyc':
+#    map => 'backend_pool',
+#    key => '*.nyc.example.com',
+#    value => 'ny-pool-1',
+#    order => '2'
+#  }
+#
+#  nginx::resource::map::entry { 'backend_pool_sf':
+#    map => 'backend_pool',
+#    key => '*.sf.example.com',
+#    value => 'sf-pool-1',
+#    order => '1'
+#  }
+#
+
+define nginx::resource::map::entry (
+  String $map,
+  String $key,
+  String $value,
+  String $order                = $key
+) {
+
+  if ! defined(Nginx::Resource::Map[$map]) {
+    fail("No such map ${map} defined")
+  }
+
+  if (getparam(Nginx::Resource::Map[$map], "mappings") != "") {
+    fail("Map ${map} has \$mappings defined, cannot use entry.")
+  }
+
+  concat::fragment { "${::nginx::conf_dir}/conf.d/${key}":
+    target => "${::nginx::conf_dir}/conf.d/${map}-map.conf",
+    content => "  ${key} ${value};\n",
+    order => "5_${order}"
+  }
+
+}
