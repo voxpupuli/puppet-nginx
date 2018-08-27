1c2060bda7baa9be223bb028cb3de4828a829db1
Tim Meusel <tim@bastelfreak.de>
2018-08-27 10:40:34 +0200

    update spec_helper_acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,9 +1,10 @@
 require 'beaker-rspec'
+require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-run_puppet_install_helper
-install_ca_certs
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
 
