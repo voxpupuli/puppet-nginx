8b285512a327f6d82d16b5f2c48db15f0d68e735
Alex Skrenchuk <alexskr@stanford.edu>
2018-10-30 22:25:23 -0700

    linting

--- examples/upstream.pp
+++ examples/upstream.pp
@@ -1,20 +1,19 @@
 include ::nginx
 
 ::nginx::resource::upstream { 'proxypass':
-
-   ensure  => present,
-   members => {
-     'localhost:3001' => {
-       server => 'localhost',
-       port   => 3000,
-     },
-     'localhost:3002' => {
-       server => 'localhost',
-       port   => 3002,
-     },
-     'localhost:3003' => {
-       server => 'localhost',
-       port   => 3003,
-     },
-   },
- }
+  ensure  => present,
+  members => {
+    'localhost:3001' => {
+      server => 'localhost',
+      port   => 3000,
+    },
+    'localhost:3002' => {
+      server => 'localhost',
+      port   => 3002,
+    },
+    'localhost:3003' => {
+      server => 'localhost',
+      port   => 3003,
+    },
+  },
+}
