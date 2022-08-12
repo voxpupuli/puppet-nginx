3166b5819dc85619c5a4ca592d87accc928b9263
Romain Tartière <romain@blogreen.org>
2022-08-11 16:00:06 -1000

    Drop Debian 9 (EOL)

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -21,7 +21,7 @@ describe 'nginx class:' do
     pkg_cmd = 'dpkg -s nginx | grep ^Maintainer'
     pkg_remove_cmd = 'apt-get -y purge nginx nginx-common'
     pkg_match = case fact('operatingsystemmajrelease')
-                when '9', '10'
+                when '10'
                   %r{Debian Nginx Maintainers}
                 when '18.04', '20.04'
                   %r{Ubuntu Developers}
