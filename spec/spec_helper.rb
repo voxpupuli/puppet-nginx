9cc6612d6d9e77b4de2065d48fe680b3816031d8
Tim Meusel <tim@bastelfreak.de>
2022-10-02 16:04:06 +0200

    modulesync 5.3.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ ENV['COVERAGE'] ||= 'yes' if Dir.exist?(File.expand_path('../lib', __dir__))
 
 require 'voxpupuli/test/spec_helper'
 
+add_mocked_facts!
+
 if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
   facts = YAML.safe_load(File.read(File.join(__dir__, 'default_module_facts.yml')))
   facts&.each do |name, value|
