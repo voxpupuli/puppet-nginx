89496d2bbdf7197ff9f32dc011b6ed8af4ddad88
Romain Tartière <romain@blogreen.org>
2023-07-05 08:56:18 -1000

    Extend supported values for Nginx::Size and Nginx::Time
    Allow raw integers and add support for combination of multiple units.

--- spec/type_aliases/time_spec.rb
+++ spec/type_aliases/time_spec.rb
@@ -10,10 +10,17 @@ describe 'Nginx::Time' do
   it { is_expected.to allow_value('1d') }
   it { is_expected.to allow_value('1M') }
   it { is_expected.to allow_value('1y') }
+  it { is_expected.to allow_value('1y 2M 3w 4d 5h 6m 7ms') }
+  it { is_expected.to allow_value(1) }
+  it { is_expected.to allow_value(10) }
+  it { is_expected.to allow_value('1') }
+  it { is_expected.to allow_value('10') }
 
   it { is_expected.not_to allow_value(:undef) }
-  it { is_expected.not_to allow_value(1) }
-  it { is_expected.not_to allow_value(10) }
+  it { is_expected.not_to allow_value(-1) }
+  it { is_expected.not_to allow_value(-10) }
+  it { is_expected.not_to allow_value('-1') }
+  it { is_expected.not_to allow_value('-10') }
   it { is_expected.not_to allow_value('') }
   it { is_expected.not_to allow_value('10S') }
   it { is_expected.not_to allow_value('10.0s') }
